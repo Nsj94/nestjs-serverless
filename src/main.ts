7a9429d4e29b8ee020a25e8ca28286648a57161b
Narinder Singh <narinder.singh@thinkclear.com>
2024-05-10 13:10:08 +0530

    Intial Setup for nestjs ServerLess application locally

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import serverlessExpress from '@codegenie/serverless-express';
+import { AppModule } from './app.module';
+import { Callback, Context, Handler } from 'aws-lambda';
+
+let server: Handler;
+
+async function bootstrap(): Promise<Handler> {
+  const app = await NestFactory.create(AppModule);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
+}
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  if (!server) {
+    server = await bootstrap();
+  }
+  return server(event, context, callback);
+};
